f281a4a0325c305666ca7f96fa5b5ccaac845231
LucianB6 <lucianbaciu2@gmail.com>
2026-06-20 17:33:26 +0200

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    
+    int minDif = 999;
+    int index = 0;
+    int previousValue = 9999;
+    int minDay = -99;
+    int maxProfitDay = 999;
+
+    while (index < prices.length){
+        if (index == 0){
+            minDay = prices[index];
+            index++;
+            continue;
+        }
+
+        if (prices[index] < minDay){
+            minDay = prices[index];
+            index++;
+            continue;
+        }
+
+        else {
+            int diff = minDay - prices[index];
+
+            if (diff < maxProfitDay){
+                maxProfitDay = diff;
+            }
+            index++;
+            continue;
+        }
+    }
+
+    if (maxProfitDay != 999){
+        return maxProfitDay * -1;
+    }
+    return 0; 
+    }
+}
